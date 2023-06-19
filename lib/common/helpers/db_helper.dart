8dd9d407f9051a893daacb9fba240344997a5707
Chijioke Chika <8pluscreative@gmail.com>
2023-06-19 05:13:07 +0100

    SQL Lite

--- lib/common/helpers/db_helper.dart
+++ lib/common/helpers/db_helper.dart
@@ -1,5 +1,8 @@
 import "package:flutter/material.dart";
 import 'package:sqflite/sqflite.dart' as sql;
+import 'package:task_manager/common/models/user_model.dart';
+
+import '../models/task_model.dart';
 
 class DBHelper {
   static Future<void> createTables(sql.Database database) async {
@@ -14,5 +17,75 @@ class DBHelper {
         "id INTEGER PRIMARY KEY AUTOINCREMENT DEFAULT 0,"
         "verified INTEGER"
         ")");
+  }
+
+  static Future<sql.Database> db() async {
+    return sql.openDatabase("achieverchika", version: 1,
+        onCreate: (sql.Database database, int version) async {
+      await createTables(database);
+    });
+  }
+
+  static Future<int> createTask(Task task) async {
+    final db = await DBHelper.db();
+
+    final item = await db.insert("todos", task.toJson(),
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+
+    return item;
+  }
+
+  static Future<int> createUser(int isVerified) async {
+    final db = await DBHelper.db();
+
+    final data = {
+      'id': 1,
+      'isVerified': isVerified,
+    };
+
+    final item = await db.insert("user", data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+
+    return item;
+  }
+
+  static Future<List<Map<String, dynamic>>> getUser() async {
+    final db = await DBHelper.db();
+    return db.query('user', orderBy: 'id');
+  }
+
+  static Future<List<Map<String, dynamic>>> getTasks() async {
+    final db = await DBHelper.db();
+    return db.query('todos', orderBy: 'id');
+  }
+
+  static Future<List<Map<String, dynamic>>> getTask(int id) async {
+    final db = await DBHelper.db();
+    return db.query('todos', where: "id = ?", whereArgs: [id], limit: 1);
+  }
+
+  static Future<int> updateTask(int id, String title, String desc,
+      int isCompleted, String date, String startTime, String endTime) async {
+    final db = await DBHelper.db();
+    final data = {
+      'title': title,
+      'desc': desc,
+      'isCompleted': isCompleted,
+      'date': date,
+      'startTime': startTime,
+      'endTime': endTime
+    };
+    final results = await db.update('todos', data, where: 'id = ?', whereArgs: [id]);
+    return results;
+  }
+
+  static Future<void> deleteTask(int id) async {
+    final db = await DBHelper.db();
+
+    try {
+      db.delete('todos', where: 'id = ?', whereArgs: [id]);
+    } catch(e){
+      debugPrint("Unable to delete");
+    }
   }
 }
