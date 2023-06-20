440d40ff237450187755c60640b3b1c35a959c43
Chijioke Chika <8pluscreative@gmail.com>
2023-06-20 06:47:47 +0100

    Task Page

--- lib/features/todo/pages/update_task.dart
+++ lib/features/todo/pages/update_task.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart'
-as picker;
+    as picker;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:task_manager/common/utils/constants.dart';
@@ -11,11 +11,11 @@ import 'package:task_manager/common/widgets/height_spacer.dart';
 import 'package:task_manager/common/widgets/reusable_text.dart';
 import 'package:task_manager/features/todo/controllers/todo/todo_provider.dart';
 
-import '../../../common/models/task_model.dart';
 import '../controllers/dates/dates_provider.dart';
 
 class UpdateTask extends ConsumerStatefulWidget {
-  const UpdateTask({super.key, required this.id, required this.title, required this.desc});
+  const UpdateTask(
+      {super.key, required this.id, required this.title, required this.desc});
 
   final int id;
   final String title;
@@ -26,7 +26,8 @@ class UpdateTask extends ConsumerStatefulWidget {
 }
 
 class _UpdateTaskState extends ConsumerState<UpdateTask> {
-  final TextEditingController prevTitle = TextEditingController(text: taskTitle);
+  final TextEditingController prevTitle =
+      TextEditingController(text: taskTitle);
   final TextEditingController prevDesc = TextEditingController(text: taskDesc);
 
   @override
@@ -68,12 +69,12 @@ class _UpdateTaskState extends ConsumerState<UpdateTask> {
                     maxTime: DateTime(2024, 6, 1),
                     theme: const picker.DatePickerTheme(
                       doneStyle:
-                      TextStyle(color: AppConst.kBlueLight, fontSize: 16),
+                          TextStyle(color: AppConst.kBlueLight, fontSize: 16),
                     ), onConfirm: (date) {
-                      ref
-                          .read(datesStateProvider.notifier)
-                          .setDate(date.toString());
-                    }, currentTime: DateTime.now(), locale: picker.LocaleType.en);
+                  ref
+                      .read(datesStateProvider.notifier)
+                      .setDate(date.toString());
+                }, currentTime: DateTime.now(), locale: picker.LocaleType.en);
               },
               width: AppConst.kWidth,
               height: 52.h,
@@ -90,10 +91,10 @@ class _UpdateTaskState extends ConsumerState<UpdateTask> {
                   onTap: () {
                     picker.DatePicker.showDateTimePicker(context,
                         showTitleActions: true, onConfirm: (date) {
-                          ref
-                              .read(startTimeStateProvider.notifier)
-                              .setStartTime(date.toString());
-                        }, locale: picker.LocaleType.en);
+                      ref
+                          .read(startTimeStateProvider.notifier)
+                          .setStartTime(date.toString());
+                    }, locale: picker.LocaleType.en);
                   },
                   width: AppConst.kWidth * 0.42,
                   height: 52.h,
@@ -106,10 +107,10 @@ class _UpdateTaskState extends ConsumerState<UpdateTask> {
                   onTap: () {
                     picker.DatePicker.showDateTimePicker(context,
                         showTitleActions: true, onConfirm: (date) {
-                          ref
-                              .read(endTimeStateProvider.notifier)
-                              .setEndTime(date.toString());
-                        }, locale: picker.LocaleType.en);
+                      ref
+                          .read(endTimeStateProvider.notifier)
+                          .setEndTime(date.toString());
+                    }, locale: picker.LocaleType.en);
                   },
                   width: AppConst.kWidth * 0.42,
                   height: 52.h,
@@ -128,29 +129,28 @@ class _UpdateTaskState extends ConsumerState<UpdateTask> {
                     scheduleDate.isNotEmpty &&
                     scheduleStartTime.isNotEmpty &&
                     scheduleEndTime.isNotEmpty) {
-                  Task task = Task(
-                      title: prevTitle.text,
-                      desc: prevDesc.text,
-                      isCompleted: 0,
-                      date: scheduleDate.substring(0, 10),
-                      startTime: scheduleStartTime.substring(10, 16),
-                      endTime: scheduleEndTime.substring(10, 16),
-                      reminder: 0,
-                      repeat: "yes");
-                  ref.read(todoStateProvider.notifier).addTask(task);
+                  ref.read(todoStateProvider.notifier).updateTask(
+                        widget.id,
+                        prevTitle.text,
+                        prevDesc.text,
+                        0,
+                        scheduleDate.substring(0, 10),
+                        scheduleStartTime.substring(10, 16),
+                        scheduleEndTime.substring(10, 16),
+                      );
                   ref.read(datesStateProvider.notifier).setDate("");
                   ref.read(startTimeStateProvider.notifier).setStartTime("");
                   ref.read(endTimeStateProvider.notifier).setEndTime("");
                   Navigator.pop(context);
                 } else {
-                  print("Error creating task");
+                  print("Error updating task");
                 }
               },
               width: AppConst.kWidth,
               height: 52.h,
               color: AppConst.kLight,
               color2: AppConst.kBlueLight,
-              text: "Submit",
+              text: "Update",
             ),
           ],
         ),
